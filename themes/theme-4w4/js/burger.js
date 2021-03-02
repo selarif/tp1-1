5167e3c1ed1bf688d7aa8f2783da4b11fc73a22f
Sami El Arif <sami.elarif@hotmail.com>
2021-03-02 11:54:17 -0500

    Corrections avec le prof pt1

--- themes/theme-4w4/js/burger.js
+++ themes/theme-4w4/js/burger.js
@@ -9,7 +9,7 @@ burger.addEventListener("mousedown", function(){
     console.log(burger.id);
     // si il n'y a pas deja l'animation d'ouverture...
     if (bar1.classList.contains("ouvrirX1") == false){
-        // ---BARRE1
+       // ---BARRE1
             //ajoute l'animation d'ouverture
             bar1.classList.add("ouvrirX1");
             // enlève l'animation de fermeture
@@ -20,20 +20,37 @@ burger.addEventListener("mousedown", function(){
         // ---BARRE3
             bar3.classList.add("ouvrirX3");
             bar3.classList.remove("fermeX3");     
+        
+      /* bar1.classList.remove("fermeX1");
+       bar1.classList.add("ouvrirX1");
+       bar2.classList.remove("fermeX2");
+       bar2.classList.add("ouvrirX2");    
+       bar3.classList.remove("fermeX3");     
+       bar3.classList.add("ouvrirX3");
+        */
     }
     // si l'animation d'ouverture est déjà là..
     else{
         // ---BARRE1
-            // enleve l'animation d'ouverture
-            bar1.classList.remove("ouvrirX1");
             // ajoute l'animation de fermeture
             bar1.classList.add("fermeX1");
+            // enlève l'animation d'ouverture
+            bar1.classList.remove("ouvrirX1");
+            
         // ---BARRE2 
             bar2.classList.remove("ouvrirX2");
             bar2.classList.add("fermeX2");   
         // ---BARRE3   
-            bar2.classList.remove("ouvrirX3");
-            bar2.classList.add("fermeX3");      
+            bar3.classList.remove("ouvrirX3");
+            bar3.classList.add("fermeX3"); 
+        
+       /*bar1.classList.remove("ouvrirX1");
+       bar1.classList.add("fermeX1");
+       bar2.classList.remove("ouvrirX2");
+       bar2.classList.add("fermeX2");   
+       bar2.classList.remove("ouvrirX3");
+       bar2.classList.add("fermeX3"); */
+
     }
     
  
